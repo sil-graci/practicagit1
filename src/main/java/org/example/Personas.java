547a2c23c79b35b184eaf5b710dd76a1b480beae
sil-graci <silviagraci@gmail.com>
2023-03-28 15:21:40 -0300

    con constructor

--- src/main/java/org/example/Personas.java
+++ src/main/java/org/example/Personas.java
@@ -2,5 +2,11 @@ package org.example;
 
 public class Personas {
     private String nombre;
+    private String apellido;
 
+
+    public Personas(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
 }
