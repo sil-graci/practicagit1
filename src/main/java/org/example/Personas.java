d8b0783436e85b8734059f1b1f992242cafb2187
sil-graci <113639238+sil-graci@users.noreply.github.com>
2023-03-28 15:27:03 -0300

    Update Personas.java

--- src/main/java/org/example/Personas.java
+++ src/main/java/org/example/Personas.java
@@ -3,6 +3,7 @@ package org.example;
 public class Personas {
     private String nombre;
     private String apellido;
+    private Integer edad;
 
 
     public Personas(String nombre, String apellido) {
